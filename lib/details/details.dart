4ec42301235d8e4807878cbcfa0c33385aa96ab7
gokhandurmaz <gdurmaz1234@gmail.com>
2026-05-21 19:05:19 +0300

    Add navigation buttons

--- lib/details/details.dart
+++ lib/details/details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class DetailScreen extends StatelessWidget {
   const DetailScreen({super.key});
@@ -10,7 +11,37 @@ class DetailScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Details Page...'),
+            const Text('Details Page!!!'),
+            const SizedBox(height: 20,),
+            BackButton(
+              onPressed: () {
+                //context.pop();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: const Text("Click back button."),
+                    duration: Duration(seconds: 2),
+                    backgroundColor: Colors.green,
+                    behavior: SnackBarBehavior.floating,
+                    action: SnackBarAction(
+                      label: "Rollback", 
+                      textColor: Colors.white,
+                      onPressed: () {
+                        context.pop();
+                      }
+                    ),
+                  )
+                );
+              
+              },
+            ),
+          
+            ElevatedButton(
+              onPressed: () {
+
+              }, 
+              child: const Text("Hello..."),
+
+            )
           ],
         ),
       ),
